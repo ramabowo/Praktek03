22b91659de396c680f69b7a1e48cf8dd68ab29a3
Akhmad Ramadhani <rama63190@gmail.com>
2023-05-27 11:16:13 +0800

    Instacne, buat atribut dan panggil method

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -5,6 +5,13 @@
 package praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-Kupu";
+        s.warna="Merah";
+        s.harga=1000;
+        
+        s.cetakInfo();
+        
         
     }
 }
