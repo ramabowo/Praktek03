0718eb44121417adc011efd4de632df46102e00c
Akhmad Ramadhani <rama63190@gmail.com>
2023-05-27 11:28:43 +0800

    Method sm

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -9,7 +9,7 @@ public class SepedaAksi {
         s.setMerk("Butterfly");
         s.setWarna("Merah");
         s.setHarga(1000);
-        
+        // Gak bisa s.setJenisMotor();
         s.cetakInfo();
         
         System.out.print("Merknya \t: ");
@@ -18,5 +18,15 @@ public class SepedaAksi {
         System.out.println(s.getWarna());
         System.out.print("Harganya \t: ");
         System.out.println(s.getHarga());
+        
+        SepedaMotor sm = new SepedaMotor();
+        sm.setMerk("Hamaya");
+        sm.setWarna("Hitam");
+        sm.setHarga(10000);
+        sm.setJenisMotor("Listrik");
+        sm.cetakInfo();
+        System.out.println("Jenis \t: "+sm.getJenisMotor());
+         sm.throttle();
+
     }
 }
